48082503e9be4c5da68a14f5d11a6de6ea92785c
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2021-12-01 19:45:49 -0500

    Second Commit

--- 72Hour.Data/IdentityModels.cs
+++ 72Hour.Data/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -7,26 +10,52 @@ using Microsoft.AspNet.Identity.Owin;
 namespace _72Hour.Data
 {
     public class ApplicationUser : IdentityUser
-{
-    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
     {
-        // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-        var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
-        // Add custom user claims here
-        return userIdentity;
-    }
-}
-
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-{
-    public ApplicationDbContext()
-    : base("DefaultConnection", throwIfV1Schema: false)
-    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 
-    public static ApplicationDbContext Create()
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        return new ApplicationDbContext();
+        public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
     }
-}
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
+    }
 }
---
+++ 72Hour.Services/PostService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72Hour.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+}
